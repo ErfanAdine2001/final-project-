5e0f799794a43fd3fd4431add89d5ec67e2f39ec
erfan.adine.javadeveloper@gmail.com <erfan.adine.javadeveloper@gmail.com>
2022-01-12 17:23:28 +0330

    first commit

---
+++ src/main/java/com/example/erfan_adine_project_main/ErfanAdineProjectMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.erfan_adine_project_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErfanAdineProjectMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErfanAdineProjectMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/erfan_adine_project_main/ErfanAdineProjectMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.erfan_adine_project_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErfanAdineProjectMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
